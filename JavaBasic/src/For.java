8553aa27a2b15eb16adc76bd17251c6d42bc3b59
이호준 <juns9050@gmail.com>
2021-10-20 15:08:00 +0900

    21.10.20 if, for

---
+++ JavaBasic/src/For.java
@@ -0,0 +1,14 @@
+public class For {
+
+	public static void main(String[] args) {
+		
+		for(int i = 1; i < 4; i++) {
+			System.out.println(i);
+			
+			for(int j = 0; j < i; j++) {
+				System.out.println("¤»¤»¤»");
+			}
+		}
+		
+	}
+}
